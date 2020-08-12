c4498347f52704a40acb96e363d6335e0e41b16e
Ethan Collins <ethancollinsdev@gmail.com>
2020-08-12 18:12:35 -0500

    added travis environment files, retriggering

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ansible frontend
+          Ansible deployment testing
         </p>
         <a
           className="App-link"
